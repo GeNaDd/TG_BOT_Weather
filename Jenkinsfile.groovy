a63bfc9621c7d072dd8bdd599655ef8304725e93
GeNaDd <44873704+GeNaDd@users.noreply.github.com>
2024-03-16 19:17:58 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
         NAME_PROJECT = 'tgbotweatherGenDevBY'
         
         NAME_IMAGE_DEV = 'gendevbydocker/gendevby_tg_bot_weather:latest'
+        TAG_IMAGE_PROD = 'prod'
     }
     stages {
         stage('build dockerimage') {
