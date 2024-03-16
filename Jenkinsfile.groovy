d0fcf4eec2231a7c099dc01ed64ffc6c0f07c8a0
GeNaDd <44873704+GeNaDd@users.noreply.github.com>
2024-03-17 01:07:19 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,7 @@ pipeline {
     agent any
     environment {
         NAME_PROJECT = 'tgbotweatherGenDevBY'
+        DOCKERHUB_CREDENTIALS = credentials('gendevbydocker')
         NAME_IMAGE_DEV = 'gendevbydocker/gendevby_tg_bot_weather'
     }
     stages {
@@ -9,6 +10,12 @@ pipeline {
             steps {
                 echo "===== Docker Build ====="
                 sh 'docker build -t ${NAME_IMAGE_DEV} .'
+            }
+        }
+        stage('Login DockerHUB') {
+            steps {
+                echo "===== Login to DockerHUB ====="
+                sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
             }
         }
         stage('Docker Push Image') {
@@ -20,6 +27,11 @@ pipeline {
         stage('test2') {
             steps {
                 echo "Hello World!"
+            }
+        }
+        post {
+            always {
+                sh 'docker logout'
             }
         }
     }
