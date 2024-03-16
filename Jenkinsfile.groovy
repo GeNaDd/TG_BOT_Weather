b19d73b74151a97304adcd19205e57358042c485
GeNaDd <44873704+GeNaDd@users.noreply.github.com>
2024-03-16 19:22:39 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,17 +2,21 @@ pipeline {
     agent any
     environment {
         NAME_PROJECT = 'tgbotweatherGenDevBY'
-        
         NAME_IMAGE_DEV = 'gendevbydocker/gendevby_tg_bot_weather:latest'
-        TAG_IMAGE_PROD = 'prod'
     }
     stages {
         stage('build dockerimage') {
             steps {
-                sh 'docker build -t gendevby_tg_bot_weather:Test .'
+                echo "Hello World!"
+            }
+        }
+    }
+    stages {
+        stage('push devimage'){
+            steps{
+                echo "Test"
             }
         }
     }
 }
-
 
